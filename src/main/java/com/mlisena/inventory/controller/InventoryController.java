55f659001bb9c19e7660070105cf90bea28232d4
Mauro Lisena <maurolisena@gmail.com>
2025-04-25 02:55:25 -0300

    Initial configuration and Creating endpoint that verify product inventory stock.

---
+++ src/main/java/com/mlisena/inventory/controller/InventoryController.java
@@ -0,0 +1,25 @@
+package com.mlisena.inventory.controller;
+
+import com.mlisena.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    public ResponseEntity<Boolean> isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(inventoryService.isInStock(skuCode, quantity));
+    }
+}
---
+++ src/main/java/com/mlisena/inventory/entity/Inventory.java
@@ -0,0 +1,22 @@
+package com.mlisena.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String skuCode;
+    Integer quantity;
+}
