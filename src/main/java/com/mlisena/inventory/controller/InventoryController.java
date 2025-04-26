0181f0eaa81d32f22591965894167badaf68d2e6
Mauro Lisena <maurolisena@gmail.com>
2025-04-26 03:43:56 -0300

    Refactor to controller and services with request validations

--- src/main/java/com/mlisena/inventory/controller/InventoryController.java
+++ src/main/java/com/mlisena/inventory/controller/InventoryController.java
@@ -1,7 +1,11 @@
 package com.mlisena.inventory.controller;
 
 import com.mlisena.inventory.service.InventoryService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
+@RefreshScope
 public class InventoryController {
 
     private final InventoryService inventoryService;
 
     @GetMapping
-    public ResponseEntity<Boolean> isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(inventoryService.isInStock(skuCode, quantity));
+    public ResponseEntity<Boolean> isInStock(
+            @RequestParam @NotBlank String skuCode,
+            @RequestParam @Positive @NotNull Integer quantity
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(inventoryService.isInStock(skuCode, quantity));
     }
 }
--- src/main/java/com/mlisena/inventory/service/InventoryService.java
+++ src/main/java/com/mlisena/inventory/service/InventoryService.java
@@ -2,15 +2,20 @@ package com.mlisena.inventory.service;
 
 import com.mlisena.inventory.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     public boolean isInStock(String skuCode, Integer quantity) {
-        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        log.info("Checking stock for SKU code: {} with quantity: {}", skuCode, quantity);
+        boolean isInStock = inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        log.info("SKU code {} has stock: {}", skuCode, isInStock);
+        return isInStock;
     }
 }
