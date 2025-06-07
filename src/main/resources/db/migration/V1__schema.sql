8efa42bf2924a0f263c37a9ccab9caedb419d693
Mauro Lisena <maurolisena@gmail.com>
2025-06-07 11:06:54 -0300

    Change create user sentence

--- src/main/resources/db/migration/V1__schema.sql
+++ src/main/resources/db/migration/V1__schema.sql
@@ -1,11 +1,11 @@
 -- Verifica la base de datos
 SHOW DATABASES;
 
--- Verifica que el usuario exista
-SELECT user, host FROM mysql.user WHERE user = 'mlisena';
-
-GRANT ALL PRIVILEGES ON inventory_db.* TO 'mlisena'@'%' IDENTIFIED BY 'A8424628';
+-- Verifica o crea el usuario
+CREATE USER IF NOT EXISTS 'mlisena'@'%' IDENTIFIED BY 'A8424628';
+GRANT ALL PRIVILEGES ON inventory_db.* TO 'mlisena'@'%';
 FLUSH PRIVILEGES;
+
 
 -- Verifica que tenga permisos sobre la base de datos
 SHOW GRANTS FOR 'mlisena'@'%';
